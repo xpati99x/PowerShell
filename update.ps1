d491d736684f22bae76f0421e1ef405571bfd58f
Patrick Grahm <patrick.grahm@gmx.de>
2020-10-13 16:12:07 +0200

    Create update.ps1

---
+++ update.ps1
@@ -0,0 +1,30 @@
+﻿function Save-GitHubRepository
+{
+    param(
+        [Parameter(Mandatory)]
+        [string]$Owner,
+
+        [Parameter(Mandatory)]
+        [string]$Repository,
+
+        [Parameter(Mandatory)]
+        [string]$Branch,
+
+        [Parameter()]
+        [string]$DestinationPath
+    )
+
+    $url = "https://github.com/$Owner/$Project/archive/$Branch.zip"
+    $start_time = Get-Date
+
+    $wc = New-Object System.Net.WebClient
+    $wc.DownloadFile($url, ($DestinationPath + "\download.zip"))
+
+    Write-Host "Time taken: $((Get-Date).Subtract($start_time).TotalSeconds) second(s)" 
+}
+
+$Downloadpfad = (Get-Location)
+
+Save-GitHubRepository xpati99x PowerShell master $Downloadpfad
+
+pause
