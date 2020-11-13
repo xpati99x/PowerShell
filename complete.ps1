eb62c6b4196c17fe5c95ebb7d276310011c3baba
xpati99x <54804914+xpati99x@users.noreply.github.com>
2020-11-13 09:52:08 +0100

    Update complete.ps1

--- complete.ps1
+++ complete.ps1
@@ -20,8 +20,18 @@ function getExcel {
 
     #open file 
     $FilePath = 'C:\cont\required\Makro.xlsm' #<------- Change this!!! 
+    $workbook = $excel.Workbooks.Open($FilePath)  
 
     #access the Application object and run a macro $app = $excel.Application 
+    $excel.Application.Run("TelefonDatShit") #<------- Change this!!! 
+    $excel.Quit()     
+
+    #Popup box to show completion - you would remove this if using task scheduler 
+    $wshell = New-Object -ComObject Wscript.Shell $wshell.Popup("Operation Completed",0,"Done",0x1)  
+}
+
+function convExToCSV {
+    
 }
 
 function split {
@@ -61,6 +71,10 @@ function addtopline {
     }
 }
 
+#Exceldatei erzeugen und auf Beendigung warten
+getExcel
+Get-Job | Wait-Job
+#Dateien aufteilen und auf Beendigung warten
 split
 Get-Job | Wait-Job
 #Kopfzeile hinzufügen
